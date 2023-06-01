77683f52cf6f7ad0152332c6e562f1108209032b
Wa1iD <wyachou95@gmail.com>
2023-06-01 13:25:35 +0200

    feat: spacer page

---
+++ pages/spacer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="spacer"></div>
+  <header>
+    <nav>
+      <ul>
+        <li><NuxtLink href="/default-ssr">Default SSR</NuxtLink></li>
+        <li><NuxtLink href="/ssr-lazy">SSR Lazy</NuxtLink></li>
+        <li><NuxtLink href="/minimize-payload">Minimize Payload</NuxtLink></li>
+        <li><NuxtLink href="/client">Client</NuxtLink></li>
+        <li><NuxtLink href="/preload">SSG (caveat)</NuxtLink></li>
+        <li><NuxtLink href="/isr">ISR</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
